51a19fd5860589ef3cd50c75ba0c88b1e3853961
Jan <MapManagement@protonmail.com>
2025-12-01 12:43:57 +0100

    Add tests for getUserById and deleteUser endpoint

--- application/src/test/java/de/seuhd/campuscoffee/tests/system/UsersSystemTests.java
+++ application/src/test/java/de/seuhd/campuscoffee/tests/system/UsersSystemTests.java
@@ -1,12 +1,16 @@
 package de.seuhd.campuscoffee.tests.system;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
 
+import de.seuhd.campuscoffee.api.dtos.UserDto;
 import de.seuhd.campuscoffee.domain.model.User;
 import de.seuhd.campuscoffee.domain.tests.TestFixtures;
 import static de.seuhd.campuscoffee.tests.SystemTestUtils.Requests.userRequests;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UsersSystemTests extends AbstractSysTest {
 
@@ -19,6 +23,35 @@ public class UsersSystemTests extends AbstractSysTest {
         assertEqualsIgnoringIdAndTimestamps(createdUser, userToCreate);
     }
 
-    // TODO: Add at least two additional tests for user operations
+    @Test
+    void getUserById() {
+        List<User> createdUserList = TestFixtures.createUsers(userService);
+        User createdUser = createdUserList.getFirst();
+
+        User retrievedUser = userDtoMapper.toDomain(
+                userRequests.retrieveById(createdUser.id()));
+
+        assertEqualsIgnoringTimestamps(retrievedUser, createdUser);
+    }
+
+    @Test
+    void deleteUser() {
+        List<User> createdUserList = TestFixtures.createUsers(userService);
+        User userToDelete = createdUserList.getFirst();
+        Objects.requireNonNull(userToDelete.id());
+
+        List<Integer> statusCodes = userRequests
+                .deleteAndReturnStatusCodes(List.of(userToDelete.id(), userToDelete.id()));
+
+        assertThat(statusCodes)
+                .containsExactly(HttpStatus.NO_CONTENT.value(), HttpStatus.NOT_FOUND.value());
+
+        List<Long> remainingUserIds = userRequests.retrieveAll()
+                .stream()
+                .map(UserDto::id)
+                .toList();
+        assertThat(remainingUserIds)
+                .doesNotContain(userToDelete.id());
+    }
 
 }
